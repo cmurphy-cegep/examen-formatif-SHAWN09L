2df65081eb65df745eb67760be7e5ef1f635471f
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-08-05 13:26:07 -0400

    Question 2

--- Q2/Facade.java
+++ Q2/Facade.java
@@ -1,22 +1,32 @@
+/*
+ * Copyright (c) 2024. Shawn Lahaie, tous droits réservés.
+ * Créé le 2024-08-05 13 h 25
+ */
 
 public class OrderFacade {
-    /**
-         * Complétez */ 
-    private Stock stock;
-    private Payment payment;
-    private Shipping shipping;
+	/**
+	 * Complétez
+	 */
+	private final Stock stock;
+	private final Payment payment;
+	private final Shipping shipping;
 
-    public OrderFacade() {
-        /**
-         * Complétez */ 
-        stock = new Stock();
-        payment = new Payment();
-        shipping = new Shipping();
-    }
+	public OrderFacade() {
+		/**
+		 * Complétez */
+		stock = new Stock();
+		payment = new Payment();
+		shipping = new Shipping();
+	}
 
-    public void placeOrder(String item, int quantity, String paymentDetails, String address) {
-        /**
-         * Complétez */ 
-        
-    }
+	public void placeOrder(String item, int quantity, String paymentDetails, String address) {
+		/**
+		 * Complétez */
+		System.out.println("Placing order for: " + item);
+		stock.checkStock(item, quantity);
+		payment.processPayment(paymentDetails);
+		shipping.arrangeShipping(item, quantity, address);
+		System.out.println("Order placed successfully.");
+
+	}
 }
