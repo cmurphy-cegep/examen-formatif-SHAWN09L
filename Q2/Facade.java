9d4826d88494abd8411961abe568cd906ca907a3
SHAWN09L <156237935+SHAWN09L@users.noreply.github.com>
2024-08-05 13:20:23 -0400

    Question 2

--- Q2/Facade.java
+++ Q2/Facade.java
@@ -2,14 +2,21 @@
 public class OrderFacade {
     /**
          * Complétez */ 
+    private Stock stock;
+    private Payment payment;
+    private Shipping shipping;
 
     public OrderFacade() {
         /**
          * Complétez */ 
+        stock = new Stock();
+        payment = new Payment();
+        shipping = new Shipping();
     }
 
     public void placeOrder(String item, int quantity, String paymentDetails, String address) {
         /**
          * Complétez */ 
+        
     }
 }
